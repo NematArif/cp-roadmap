f092d3de504f638b69609522d11241bdf67fd774
Nemat Arif <nemat.legend@gmail.com>
2025-07-14 22:45:13 -0400

    Create FenwickTree.cpp

---
+++ Templates/FenwickTree.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define LSOne(S) ((S) & (-S))
+
+typedef long long ll;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+
+class FenwickTree {
+private:
+    vi ft;
+public:
+    FenwickTree(int m) { ft.assign(m + 1, 0); }
+
+    void build(const vll &f) {
+        int m = (int)f.size() - 1;
+        ft.assign(m + 1, 0);
+        for(int i = 1;i <= m;i ++) {
+            ft[i] += f[i];
+            if(i + LSOne(i) <= m)
+                ft[i + LSOne(i)] += ft[i];
+        }
+    }
+
+    FenwickTree(const vll &f) { build(f); }
+
+    FenwickTree(int m, const vi &s) {
+        vll f(m + 1, 0);
+        for(int i = 0;i < (int)s.size();i ++)
+            ++f[s[i]];
+        build(f);
+    }
+
+    int rsq(int j) {
+        int sum = 0;
+        for(; j; j -= LSOne(j))
+            sum += ft[j];
+        return sum;
+    }
+
+    int rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
+
+    void update(int i, int v) {
+        for(; i < (int)ft.size(); i += LSOne(i))
+            ft[i] += v;
+    }
+
+    int select(ll k) {
+        int lo = 1, hi = ft.size() - 1;
+        for (int i = 0;i < 30;++ i) {
+            int mid = (lo + hi) / 2;
+            (rsq(1, mid) < k) ? lo = mid : hi = mid;
+        }
+        return hi;
+    }
+};
+
+class RUPQ {
+private:
+    FenwickTree ft;
+public:
+    RUPQ(int m) : ft(FenwickTree(m)) {}
+    void range_update(int ui, int uj, int v) {
+        ft.update(ui, v);
+        ft.update(uj + 1, -v);
+    }
+    ll point_query(int i) { return ft.rsq(i); }
+};
+
+class RURQ {
+private:
+    RUPQ rupq;
+    FenwickTree purq;
+public:
+    RURQ(int m) : rupq(RUPQ(m)), purq(FenwickTree(m)) {}
+    void range_update(int ui, int uj, int v) {
+        rupq.range_update(ui, uj, v);
+        purq.update(ui, v * (ui - 1));
+        purq.update(uj + 1, -v*uj);
+    }
+    ll rsq(int j) {
+        return rupq.point_query(j) * j -
+               purq.rsq(j);
+    }
+    ll rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
+};
+
+int main() {
+    vll f = {0, 0, 1, 0, 1, 2, 3, 2, 1, 1, 0};
+    FenwickTree ft(f);
+    printf("%lld\n", ft.rsq(1, 6));
+    printf("%d\n", ft.select(7));
+    ft.update(5, 1);
+    printf("%lld\n", ft.rsq(1, 10));
+    printf("=====\n");
+    RUPQ rupq(10);
+    RURQ rurq(10);
+    rupq.range_update(2, 9, 7);
+    rurq.range_update(2, 9, 7);
+    rupq.range_update(6, 7, 3);
+    rurq.range_update(6, 7, 3);
+
+    for (int i = 1;i <= 10;i ++)
+        printf("%d -> %lld\n", i, rupq.point_query(i));
+
+    printf("RSQ(1, 10) = %lld\n", rurq.rsq(1, 10));
+    printf("RSQ(6, 7) = %lld\n", rurq.rsq(6, 7));
+}
