0103cb2338d4d6cb0c3278686d14e3ed0c3adc0d
Nemat Arif <nemat.legend@gmail.com>
2025-07-15 14:38:40 -0400

    Update FenwickTree.cpp

--- Templates/FenwickTree.cpp
+++ Templates/FenwickTree.cpp
@@ -9,46 +9,47 @@ typedef long double ld;
 typedef vector<int> vi;
 typedef vector<ll> vll;
 
-class FenwickTree {
+class FenwickTree {                             // index 0 is not used
 private:
-    vi ft;
+    vll ft;                                     // internal FT is an array
 public:
-    FenwickTree(int m) { ft.assign(m + 1, 0); }
+    FenwickTree(int m) { ft.assign(m + 1, 0); } // create an empty FT
 
-    void build(const vll &f) {
-        int m = (int)f.size() - 1;
+    void build(const vll &f) {                  
+        int m = (int)f.size() - 1;              // note f[0] is always 0
         ft.assign(m + 1, 0);
-        for(int i = 1;i <= m;i ++) {
-            ft[i] += f[i];
-            if(i + LSOne(i) <= m)
-                ft[i + LSOne(i)] += ft[i];
+        for(int i = 1;i <= m;i ++) {            // O(m)
+            ft[i] += f[i];                      // add this value
+            if(i + LSOne(i) <= m)               // i has parent
+                ft[i + LSOne(i)] += ft[i];      // add to that parent
         }
     }
 
-    FenwickTree(const vll &f) { build(f); }
+    FenwickTree(const vll &f) { build(f); }     // create FT based on f
 
-    FenwickTree(int m, const vi &s) {
+    FenwickTree(int m, const vi &s) {           // create FT based on s
         vll f(m + 1, 0);
-        for(int i = 0;i < (int)s.size();i ++)
-            ++f[s[i]];
-        build(f);
+        for(int i = 0;i < (int)s.size();i ++)   // do the conversion first
+            ++f[s[i]];                          // in O(n)
+        build(f);                               // in O(m)
     }
 
-    int rsq(int j) {
+    int rsq(int j) {                            // returns RSQ(1, j)
         int sum = 0;
-        for(; j; j -= LSOne(j))
+        for(; j; j -= LSOne(j)) 
             sum += ft[j];
         return sum;
     }
 
-    int rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
+    int rsq(int i, int j) { return rsq(j) - rsq(i - 1); }   // inc/exclusion
 
+    // updates value of the i-th element by v (v can be +ve/inc or -ve/dec)
     void update(int i, int v) {
         for(; i < (int)ft.size(); i += LSOne(i))
             ft[i] += v;
     }
 
-    int select(ll k) {
+    int select(ll k) {                              // O(log m)
         int lo = 1, hi = ft.size() - 1;
         for (int i = 0;i < 30;++ i) {
             int mid = (lo + hi) / 2;
@@ -58,34 +59,34 @@ public:
     }
 };
 
-class RUPQ {
+class RUPQ {                                    // RUPQ variant
 private:
-    FenwickTree ft;
-public:
+    FenwickTree ft;                             // internally use PURQ FT
+public:         
     RUPQ(int m) : ft(FenwickTree(m)) {}
     void range_update(int ui, int uj, int v) {
-        ft.update(ui, v);
-        ft.update(uj + 1, -v);
-    }
-    ll point_query(int i) { return ft.rsq(i); }
+        ft.update(ui, v);                       // [ui, ui+1, ..., m] +v
+        ft.update(uj + 1, -v);                  // [uj+1, uj+2, ..., m] -v
+    }                                           // [ui, ui+1, ..., uj] +v
+    ll point_query(int i) { return ft.rsq(i); } // rsq(i) is sufficient
 };
 
-class RURQ {
-private:
-    RUPQ rupq;
-    FenwickTree purq;
+class RURQ {                                    // RURQ variant
+private:                                        // need two helper FTs      
+    RUPQ rupq;                                  // one RUPQ and
+    FenwickTree purq;                           // one PURQ
 public:
-    RURQ(int m) : rupq(RUPQ(m)), purq(FenwickTree(m)) {}
+    RURQ(int m) : rupq(RUPQ(m)), purq(FenwickTree(m)) {} // initialization
     void range_update(int ui, int uj, int v) {
-        rupq.range_update(ui, uj, v);
-        purq.update(ui, v * (ui - 1));
-        purq.update(uj + 1, -v*uj);
+        rupq.range_update(ui, uj, v);                    // [ui, ui+1, ..., uj] +v
+        purq.update(ui, v * (ui - 1));                   // -(ui-1)*v before ui
+        purq.update(uj + 1, -v*uj);                      // +(uj-ui+1)*v after uj
     }
-    ll rsq(int j) {
-        return rupq.point_query(j) * j -
-               purq.rsq(j);
+    ll rsq(int j) {                                      
+        return rupq.point_query(j) * j -                 // optimistic cancellation
+               purq.rsq(j);                              // cancellation factor
     }
-    ll rsq(int i, int j) { return rsq(j) - rsq(i - 1); }
+    ll rsq(int i, int j) { return rsq(j) - rsq(i - 1); } // standard
 };
 
 int main() {
