7d5e6d1ba51916cb22d9b690fe56a25d31466b5b
Nemat Arif <nemat.legend@gmail.com>
2025-07-15 14:25:46 -0400

    Update UFDS.cpp

--- Templates/UFDS.cpp
+++ Templates/UFDS.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 typedef vector<int> vi;
 
-class UnionFind {
+class UnionFind {                                                                   //OOP style
 private:
     vi p, rank, setSize;
     int numSets;
@@ -13,24 +13,24 @@ public:
     UnionFind(int N) {
         p.assign(N, 0);
         for (int i = 0; i < N; ++i) p[i] = i;
-        rank.assign(N, 0);
-        setSize.assign(N, 1);
-        numSets = N;
+        rank.assign(N, 0);                    // optional speed up
+        setSize.assign(N, 1);                 // optional speed up
+        numSets = N;                          // optional speed up
     }
 
     int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
     bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
-    int numDisjointSets() { return numSets; }
-    int sizeOfSet(int i) { return setSize[findSet(i)]; }
+    int numDisjointSets() { return numSets; }                                  // optional
+    int sizeOfSet(int i) { return setSize[findSet(i)]; }                        // optional
 
     void UnionSet(int i, int j) {
-        if (isSameSet(i, j)) return;
-        int x = findSet(i), y = findSet(j);
-        if (rank[x] > rank[y]) swap(x, y);
-        p[x] = y;
-        if (rank[x] == rank[y]) ++rank[y];
-        setSize[y] += setSize[x];
-        numSets--;
+        if (isSameSet(i, j)) return;                        // i and j are in the same set
+        int x = findSet(i), y = findSet(j);                 // find both rep items
+        if (rank[x] > rank[y]) swap(x, y);                  // keep x 'shorter' than y
+        p[x] = y;                                           // set x under y
+        if (rank[x] == rank[y]) ++rank[y];                  // optional speed up
+        setSize[y] += setSize[x];                           // combine set sizes at y
+        numSets--;                                          // a union reduces numSets
     }
 };
 
